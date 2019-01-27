7e9cee5d3f30f318f21519b7f4a7935e616e23d8
jennifer kerssen <jjkerssen@gmail.com>
2019-01-26 22:00:30 -0700

    initial commit

---
+++ pages/sell.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+const Sell = props => (
+    <div>
+        <p>Sell!</p>
+        <Link href='index'>
+        <a>Hey!</a>
+        </Link>
+    </div>
+)
+
+export default Sell;
